b7b8e95d2410ba14d6bcb136a0f26b81e17daca2
mista17 <95057399+mista17@users.noreply.github.com>
2022-05-21 16:13:17 +0300

    Update logo.py

--- logo.py
+++ logo.py
@@ -1,31 +1,6 @@
-from tqdm import trange
-from time import sleep
-from turtle import *
-
-def loading():
-    print('Загрузка...\n')
-    for i in trange(100):
-        sleep(0.05)
-    sleep(0.5)
-    print('\nЗагрузка завершена!')
-    
-def loading_line():
-    z = 1
-    s = 110
-    analis = ''
-    for p in range(2):
-        for q in range(56):
-            print('-' * z, ' * ', '-' * s, ' .', '-' * z)
-            z += 1
-            s -= 2
-            sleep(0.01)
-        print(' ' * (z - 4), analis)
-        for q in range(56):
-            z -= 1
-            s += 2
-            print('-' * z, ' . ', '-' * s, ' *', '-' * z)
-            sleep(0.01)
-        analis = 'Завершение работы'
-
-loading()
-loading_line()
+def logo():
+    print(' '*50,' ▀█▀ ░█▀▀█ 　 ▀▀█▀▀ ░█▀▀▀ ░█▀▀▀█ ▀▀█▀▀') 
+    print(' '*50,' ░█─ ░█─░█ 　 ─░█── ░█▀▀▀ ─▀▀▀▄▄ ─░█──')
+    print(' '*50,' ▄█▄ ─▀▀█▄ 　 ─░█── ░█▄▄▄ ░█▄▄▄█ ─░█──')
+    print(' '*70,'by  ɴᴇᴜʀᴏᴘʀᴏɢʀᴀᴍᴍᴇʀs')
+logo()
